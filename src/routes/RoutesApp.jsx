85fef2cd557180bdc4ef5cc715bb7481f1eb8232
monicajorgedev <monicajorge.dev@gmail.com>
2024-11-30 12:19:18 +0100

    resolve error in import

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from '../components/Header'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
-import ActivityCreate from '../pages/ActivityCreate'
+
 import CompanyCreate from '../pages/CompanyCreate'
 import ActivityDetail from '../pages/ActivityDetail'
 import ActivityUpdate from '../pages/ActivityUpdate'
@@ -20,7 +20,7 @@ const RoutesApp = () => {
         <Route path='/' element={< Home/>}/>
         <Route path='/login' element={< Login />}/>
         <Route path='/register' element={< CompanyCreate />}/>
-        <Route path='/activity_create' element={< ActivityCreate />}/>
+        
         <Route path='/activity/:id' element={< ActivityDetail  />}/>
         <Route path='/activity/:id/update' element={< ActivityUpdate  />}/>
         <Route path='/activities/by-company' element={< ActivitiesByCompany  />}/>
