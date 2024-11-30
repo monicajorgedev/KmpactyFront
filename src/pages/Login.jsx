d0e9cec34c3e62e37cc08451472b8c9838a9931a
monicajorgedev <monicajorge.dev@gmail.com>
2024-11-30 12:06:12 +0100

    resolve error in import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import Loginform from "src/components/forms/LoginForm";
+import Loginform from "../components/forms/Loginform";
 
 const Login = () => {
     return (
